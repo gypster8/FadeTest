59177135a2208797deb3bdad5a5465d26b34a85b
gypster8 <gianaccettamusic@gmail.com>
2024-12-01 17:41:36 -0500

    Update FadeTest.ino

--- FadeTest.ino
+++ FadeTest.ino
@@ -13,7 +13,8 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Fade
 */
 
-int led = 9;         // the PWM pin the LED is attached to
+int led = 10;  
+int led2 = 11;
 int brightness = 0;  // how bright the LED is
 int fadeAmount = 5;  // how many points to fade the LED by
 
@@ -27,6 +28,7 @@ void setup() {
 void loop() {
   // set the brightness of pin 9:
   analogWrite(led, brightness);
+  analogWrite(led2, birghtness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
